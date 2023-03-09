99769c4182f76995cfcc316b32e674aabc174020
Leonardo M. Miranda <leonardomichalskim@gmail.com>
2023-03-09 20:34:05 -0300

    use the latest docker image version for testing

--- install_tests/run_tests.sh
+++ install_tests/run_tests.sh
@@ -1,18 +1,18 @@
 # pyvirtualdisplay and easyprocess test
-docker run --rm -v ${PWD}:/curr -w /curr leommiranda/pyautogui:libpci-and-libegl python3 test_pyvirtualdisplay.py
+docker run --rm -v ${PWD}:/curr -w /curr leommiranda/pyautogui python3 test_pyvirtualdisplay.py
 
 # firefox test
-docker run --rm -v ${PWD}:/curr -w /curr leommiranda/pyautogui:libpci-and-libegl python3 test_firefox.py
+docker run --rm -v ${PWD}:/curr -w /curr leommiranda/pyautogui python3 test_firefox.py
 
 # pyautogui test (no selenium)
-docker run --rm -v ${PWD}:/curr -w /curr leommiranda/pyautogui:libpci-and-libegl python3 test_pyautogui.py
+docker run --rm -v ${PWD}:/curr -w /curr leommiranda/pyautogui python3 test_pyautogui.py
 
 # selenium only test (no easyprocess)
-docker run --rm -v ${PWD}:/curr -w /curr leommiranda/pyautogui:libpci-and-libegl bash -c "
+docker run --rm -v ${PWD}:/curr -w /curr leommiranda/pyautogui bash -c "
     Xvfb :1 -screen 0 1024x768x16 &> xvfb.log  & \
     export DISPLAY=:1.0; \
     python3 test_selenium.py;
 "
 
 # pyautogui and selenium test (no easyprocess)
-docker run --rm -v ${PWD}:/curr -w /curr leommiranda/pyautogui:libpci-and-libegl python3 test_selenium_and_pyautogui.py
+docker run --rm -v ${PWD}:/curr -w /curr leommiranda/pyautogui python3 test_selenium_and_pyautogui.py
